cd96e585300963823ea4fc6517dd68d6254d2965
VulcanWM <logmedha09@gmail.com>
2026-03-04 20:38:01 +0000

    fixing windows not showing emojis

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "game.h"
 
 int main() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
     Game g;
     g.play();
     return 0;
